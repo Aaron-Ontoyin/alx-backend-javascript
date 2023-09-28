0cb95ca2d04caab6c8bb2882f323315cda5e5507
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-09-28 02:11:46 +0000

    task 3 done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils'
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((output) => {
-      console.log(`${output.body} ${output.firstName} ${output.lastName}`);
+      console.log(`${output[0].body} ${output[1].firstName} ${output[1].lastName}`);
     }).catch(() => {
       console.log('Signup system offline');
     });
