95f24f809ec5b85b0da049cfb6f728171da2b9d4
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-09-30 14:20:33 +0000

    task 5 done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,20 +1,14 @@
 export default class Building {
-  constructor(sqft) {
-    if (typeof this.evacuationWarningMessage !== 'function'
-      && this.constructor !== 'Building') {
-      throw new Error('Error: Class extending Building must override evacuationWarningMessage');
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 
-    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
-    this._sqft = sqft;
   }
 }
