d10d050b6a7a1d344ff5acd5280cb75199fc9437
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-04 01:04:33 +0000

    task 4 done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,5 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const cityStudents = students.filter((s) => s.city === city);
+  const cityStudents = students.filter((s) => s.location === city);
   return cityStudents.map((s) => {
     const grade = newGrades.filter((g) => g.studentId === s.id);
     const studentGrade = grade.length === 1 ? grade[0].grade : 'N/A';
