e0ef4998a6fca21f0a75d77d8eca69acbcda17d4
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-04 02:50:16 +0000

    task 100 done

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,14 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endPoint) {
-  if (weakMap.get(endPoint) >= 5) {
-    throw new Error('Endpoint load is high');
-  }
-
   const endPointInWeakMap = weakMap.get(endPoint);
   if (!endPointInWeakMap) {
     weakMap.set(endPoint, 1);
   } else {
     weakMap.set(endPoint, endPointInWeakMap + 1);
+  }
+
+  if (endPointInWeakMap + 1 >= 5) {
+    throw new Error('Endpoint load is high');
   }
 }
