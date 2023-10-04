281aa75c7cbf203b0062b401080e28ce00910bd8
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-10-04 02:06:52 +0000

    task 8 done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
   const output = new Set();
   set.forEach((el) => {
     if (el.startsWith(startString)) {
       output.add(el.slice(startString.length));
     }
   });
-  return output.join('-');
+  return [...output].join('-');
 }
