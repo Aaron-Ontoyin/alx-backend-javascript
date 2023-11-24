4ce4f45af71a7b2493cb83b8bf508c5b83df995d
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-24 01:55:55 +0000

    task 1

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', function () {
+    it('should return 4 when inputs are 1.6 and 2.4', function () {
+        assert.strictEqual(calculateNumber(1.6, 2.4), 4);
+    });
+    it('should return 6 when inputs are 2.5 and 2.5', function () {
+        assert.strictEqual(calculateNumber(2.5, 2.5), 6);
+    });
+    it('should return -4 when inputs are -2.5 and -2.5', function () {
+        assert.strictEqual(calculateNumber(-2.5, -2.5), -4);
+    });
+    it('should return 0 when inputs are 0 and 0', function () {
+        assert.strictEqual(calculateNumber(0, 0), 0);
+    });
+});
