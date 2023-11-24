247b0c3b18468259f5a0a1e2c6df3f8ceb032825
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-24 02:30:11 +0000

    task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -14,4 +14,31 @@ describe('calculateNumber', function () {
     it('should return 0 when type is SUM and inputs are 0 and 0', function () {
         assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
     });
+    it('should return Error when type is DIVIDE and inputs are 1 and 0', function () {
+        assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+    });
+    it(`checking if numbers round`, function () {
+        const res = calculateNumber("SUM", 1, 2);
+        assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function () {
+        const res = calculateNumber("SUBTRACT", 1.4, 2.2);
+        assert.strictEqual(res, -1);
+    });
+    it(`checking if numbers round`, function () {
+        const res = calculateNumber("SUBTRACT", 4.9, 2.7);
+        assert.strictEqual(res, 2);
+    });
+    it(`checking if numbers round`, function () {
+        const res = calculateNumber("DIVIDE", 4, 2);
+        assert.strictEqual(res, 2);
+    });
+    it(`checking if numbers round`, function () {
+        const res = calculateNumber("DIVIDE", 1.7, 0);
+        assert.strictEqual(res, "Error");
+    });
+    it(`checking if numbers round`, function () {
+        const res = calculateNumber("DIVIDE", 1.4, 4.6);
+        assert.strictEqual(res, 0.2);
+    });
 });
