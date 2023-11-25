d9ee4642a92eb816b22d6a2ebad792bceca72924
Aaron Ontoyin Yin <aarononto909@gmail.com>
2023-11-25 00:09:09 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,16 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-    process.stdout.write(`Your name is: ${name}\n`);
-    if (rl.isTTY) {
-        process.stdout.write('This important software is now closing\n');
-    }
-    rl.close();
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
 
